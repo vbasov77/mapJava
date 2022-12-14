e81a9c09b9fa9efae05a0bc67083169e65062a24
Vbasov <0120912@mail.ru>
2022-12-14 11:09:21 +0300

    add

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Map<String, List<String>> phoneBook = new HashMap<>();
+        phoneBook.put("Иванов", List.of("+7(999)9999999", "+7(999)8888888", "+7(999)7777777"));
+        phoneBook.put("Петров", List.of("+7(999)5456789", "+7(999)2344323"));
+        phoneBook.put("Сидоров",List.of("+7(555)3456776"));
+
+        System.out.println(phoneBook);
+    }
+}
